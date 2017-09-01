d89da973bbad35440a5b6eb7e362f6b703a3c5d8
Henrique Mascalhusk <hmascalhusk@id.uff.br>
2017-09-01 14:51:26 -0300

    Zerando a terceira e quarta linha

--- tarefa3.c
+++ tarefa3.c
@@ -56,7 +56,27 @@ int main()
 
 	}
 
-	//escrevendo a matriz
+	//Rescrevendo a matriz
+	printf ("Reescrevendo a Matriz:\n");
+	for (i=0; i<lin; i++)
+   	{	 for ( j=0; j<col; j++ )
+    		{
+       			printf ("%.1f \t", mat[ i ][ j ]);
+    		}
+		printf("\n\n");	
+ 	}
+
+
+	//zerando terceira linha
+	printf("Zerando a segunda e terceira linha:\n");
+	for (i=0; i<col; i++)
+	{
+		mat[2][0] = mat[2][0] - (mat[2][0]/mat[0][0])*mat[0][1];
+		mat[3][0] = mat[3][0] - (mat[3][0]/mat[0][0])*mat[0][1];
+
+	}
+	
+	//Rescrevendo a matriz
 	printf ("Reescrevendo a Matriz:\n");
 	for (i=0; i<lin; i++)
    	{	 for ( j=0; j<col; j++ )
